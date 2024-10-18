8c9f2aeebe38aa92e79fb8b51c0edae956456ca2
888Saint <psdas32@gmail.com>
2024-10-18 19:48:53 +0530

    Update Main.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -2,15 +2,15 @@ package com.driver;
 
 public class Car extends Vehicle {
     private int wheels;
+    private String type;
     private int doors;
     private int gears;
     private boolean isManual;
     private int currentGear;
-    private String type;
     private int seats;
 
-    // Constructor
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        //Hint: Car extends Vehicle
         super(name);  // Call the Vehicle constructor
         this.wheels = wheels;
         this.doors = doors;
@@ -21,14 +21,13 @@ public class Car extends Vehicle {
         this.currentGear = 1;  // Initialize with gear 1
     }
 
-    // Method to change the gear
-    public void changeGear(int newGear) {
+    public void changeGear(int newGear){
+
         this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
+        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
-    // Method to change speed and direction
-    public void changeSpeed(int newSpeed, int newDirection) {
+    public void changeSpeed(int newSpeed, int newDirection){
         move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,17 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        // Create an F1 car object
         F1 mercedes = new F1("mercedes", false);
-        
-        // Test the functionality of F1 car
-        mercedes.move(40, 0);  // Move the car initially
-        mercedes.accelerate(20);  // Accelerate the car
-        mercedes.accelerate(-60);  // Decelerate the car
-
-        // Test the Boat class
-        Boat yacht = new Boat("Yacht", 20);
-        System.out.println("Boat name: " + yacht.getVehicleName());
-        System.out.println("Boat capacity: " + yacht.getVehicleCapacity());
+        mercedes.move(40, 0);
+        mercedes.accelerate(20);
+        mercedes.accelerate(-60);
     }
 }
