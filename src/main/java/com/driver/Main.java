2db9dffd1728f764dc1ada122de470c85b89c391
888Saint <psdas32@gmail.com>
2024-10-21 16:54:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,13 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-<<<<<<< HEAD
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
-=======
-        
+
         // Test the functionality of F1 car
         mercedes.move(40, 0);  // Move the car initially
         mercedes.accelerate(20);  // Accelerate the car
         mercedes.accelerate(-60);  // Decelerate the car
->>>>>>> 17d5f185702cc50b6f8c6e1c0ce429e930dbbe82
     }
 }
